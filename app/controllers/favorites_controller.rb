f8ef526236bff3dcae4beba8e87d95311b3843df
satoshi yamada <yasain0852@yahoo.co.jp>
2019-12-18 11:50:06 +0900

    herokuデプロイ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,17 @@
 class FavoritesController < ApplicationController
   def create
+    binding.pry
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
+    redirect_to pictures_url, notice: "#{favorite.pictiure.user.name}さんのブログをお気に入り解除しました"
   end
 
   def index
-    @favorites = current_user.favorite_pictures
+    @favorite_pictures = current_user.favorite_pictures
   end
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,4 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :picture
-  mount_uploader :image, ImageUploader
 end
