d835f2cc7af5ab7eadc1bcb4c4f11a8f21ca5d32
satoshi yamada <yasain0852@yahoo.co.jp>
2019-12-20 11:08:02 +0900

    修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to pictures_url, notice: "#{favorite.pictiure.user.name}さんのブログをお気に入り解除しました"
+    redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
   end
 
   def index
